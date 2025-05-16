f2ce9e4caaed70e93b146a26d4d9482610088d36
albertchouforces <albert.chou.forces@gmail.com>
2025-05-16 13:48:43 -0700

    Update KnotIllustrations.tsx

--- src/components/KnotIllustrations.tsx
+++ src/components/KnotIllustrations.tsx
@@ -74,17 +74,17 @@ const BowlineIllustration: FC<StepIllustrationProps> = ({ stepNumber, className 
   const getStepImageUrl = (step: number) => {
     switch (step) {
       case 1:
-        return "public/images/Bowline1.png";
+        return "/images/Bowline1.png";
       case 2:
-        return "public/images/Bowline2.png";
+        return "/images/Bowline2.png";
       case 3:
-        return "public/images/Bowline3.png";
+        return "/images/Bowline3.png";
       case 4:
-        return "public/images/Bowline4.png";
+        return "/images/Bowline4.png";
       case 5:
-        return "public/images/Bowline5.png";
+        return "/images/Bowline5.png";
       default:
-        return "public/images/Bowline5.png";
+        return "/images/Bowline5.png";
     }
   };
 
@@ -110,13 +110,13 @@ const CloveHitchIllustration: FC<StepIllustrationProps> = ({ stepNumber, classNa
   const getStepImageUrl = (step: number) => {
     switch (step) {
       case 1:
-        return "public/images/clovehitch1.jpg";
+        return "/images/clovehitch1.jpg";
       case 2:
-        return "public/images/clovehitch2.jpg";
+        return "/images/clovehitch2.jpg";
       case 3:
-        return "public/images/clovehitch3.jpg";
+        return "/images/clovehitch3.jpg";
       default:
-        return "public/images/clovehitch3.jpg";
+        return "/images/clovehitch3.jpg";
     }
   };
 
@@ -140,15 +140,15 @@ const CleatHitchIllustration: FC<StepIllustrationProps> = ({ stepNumber, classNa
   const getStepImageUrl = (step: number) => {
     switch (step) {
       case 1:
-        return "public/images/cleathitch1.jpg";
+        return "/images/cleathitch1.jpg";
       case 2:
-        return "public/images/cleathitch2.jpg";
+        return "/images/cleathitch2.jpg";
       case 3:
-        return "public/images/cleathitch3.jpg";
+        return "/images/cleathitch3.jpg";
       case 4:
-        return "public/images/cleathitch4.jpg";
+        return "/images/cleathitch4.jpg";
       default:
-        return "public/images/cleathitch4.jpg";
+        return "/images/cleathitch4.jpg";
     }
   };
 
