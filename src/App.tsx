deb2ccc6e4a9b0129c8a380c4ef3ea021e5e2096
albertchouforces <albert.chou.forces@gmail.com>
2025-06-05 10:40:36 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export function App() {
         <footer className="mt-12 py-6 border-t border-gray-200">
           <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
             <p>KnotCraft - Master essential naval knots with interactive tutorials</p>
-            <p className="mt-1">All knot illustrations courtesy of Wikipedia</p>
+            <p className="mt-3 pt-3 border-t border-gray-100">This is a professional development learning project. For more information please contact the Learning Support Centre Product Development Lead (Pacific) at <a href="mailto:joshua.hawthorne@ecn.forces.gc.ca" className="text-blue-500 hover:underline">joshua.hawthorne@ecn.forces.gc.ca</a></p>
           </div>
         </footer>
       </Router>
